97be281a900217d219c44699342c0a891f98d3ab
Enes Himmet Eraslan <eneshimmet@outlook.com>
2022-12-04 15:02:15 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("Bir sayı giriniz: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bir sayı daha giriniz: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sayıların çarpımı: " + num1 * num2 );
+            Console.ReadKey();
+        }
+    }
+}
